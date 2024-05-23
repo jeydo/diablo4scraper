6ca66f3b51339c480f6f77376f578562d3c8297c
Jey <your@email.com>
2024-05-24 00:43:00 +0200

    remove cors because of nginx reverse proxy

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ const scraper = require('./scraper.js')
 const builder = require('./builder.js')
 
 app.use(express.json())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.post('/scrapebuild', (req, res) => {
     const url = req.body.url;
